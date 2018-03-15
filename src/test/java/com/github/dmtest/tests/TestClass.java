c693e01786031997bc1c3386d44b67a7a6594b83
Dmitry Piskunov <dm.test@mail.ru>
2018-03-15 10:35:19 +0600

    written a simple test; Allure is connected

---
+++ src/test/java/com/github/dmtest/tests/TestClass.java
@@ -0,0 +1,11 @@
+package com.github.dmtest.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass {
+    @Test
+    public static void simpleTest() {
+        Assert.assertTrue(true);
+    }
+}
